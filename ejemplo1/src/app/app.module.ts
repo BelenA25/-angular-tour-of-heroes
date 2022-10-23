bade51929b8d7367cf9e5abdc911dbda058a35e5
Belen <beckiar23@gmail.com>
2022-10-23 10:21:52 -0200

    Nuevo

---
+++ ejemplo1/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { InicioComponent } from './paginas/inicio/inicio.component';
+import { ServiciosComponent } from './paginas/servicios/servicios.component';
+import { TrabajosComponent } from './paginas/trabajos/trabajos.component';
+import { EquipoComponent } from './paginas/equipo/equipo.component';
+import { PaginaNoEncontradaComponent } from './paginas/pagina-no-encontrada/pagina-no-encontrada.component';
+import { TablaComponent } from './elementos/tabla/tabla.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    InicioComponent,
+    ServiciosComponent,
+    TrabajosComponent,
+    EquipoComponent,
+    PaginaNoEncontradaComponent,
+    TablaComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
